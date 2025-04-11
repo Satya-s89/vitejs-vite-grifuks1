56414dfea1b86ca9ab12c1c995ac9f2425900e06
Satya-s89 <kaki.harisatya.s89@kalvium.community>
2025-04-11 13:24:07 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
-// write button card here
+import React from 'react';
+import './button.css'; 
+
+const ViewProductButton = () => {
+  return (
+    <button className="view-product-btn">View Product</button>
+  );
+};
+
+export default ViewProductButton;
